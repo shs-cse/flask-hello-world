de6f0fa88dffcaa70b19c0a6a64c0179c45980f4
Shadman Shahriar <shadman.shahriar@bracu.ac.bd>
2025-10-15 05:42:23 +0600

    seupt.sh add line for always https

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ echo "server {
         proxy_pass http://127.0.0.1:8000;
         proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }" > "/etc/nginx/sites-enabled/flask_app"
 sudo unlink /etc/nginx/sites-enabled/default
